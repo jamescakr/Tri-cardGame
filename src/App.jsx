6db41f75505b65416924603b201f7970f3c247f0
jamesjo <jamescakr@gmail.com>
2025-04-02 00:25:58 -0600

    complete card game

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,24 @@ import fireMage from "../src/assets/fire-mage.png";
 import waterMage from "../src/assets/water-mage.png";
 import droughtMage from "../src/assets/drought-mage.png";
 import fireBg from "../src/assets/fire-bg.webp";
-import droughtBg from "../src/assets/drought1.webp";
-import waterBg from "../src/assets/water1.webp";
+import droughtBg from "../src/assets/drought-bg.webp";
+import waterBg from "../src/assets/water-bg.webp";
+
 import "animate.css";
 
 const choices = {
   fire: {
-    name: "fireMage",
+    name: "fire",
     img: fireMage,
     bg: fireBg,
   },
   drought: {
-    name: "dryCard",
+    name: "drought",
     img: droughtMage,
     bg: droughtBg,
   },
   water: {
-    name: "waterCard",
+    name: "water",
     img: waterMage,
     bg: waterBg,
   },
@@ -30,24 +31,50 @@ const App = () => {
   const [userSelect, setUserSelect] = useState(null);
   const [computerSelect, setComputerSelect] = useState(null);
   const [bgImage, setBgImage] = useState(null);
+  const [cardResetKey, setCardResetKey] = useState(0);
+  const [result, setResult] = useState("");
 
-  const userPlay = (userChoice) => {
-    setUserSelect(choices[userChoice]);
-    setBgImage(choices[userChoice].bg);
+  const whoIsWin = (userPickObj, computerPickObj) => {
+    if (userPickObj.name === computerPickObj.name) {
+      return "DRAW";
+    } else if (userPickObj.name === "fire" && computerPickObj.name === "drought") {
+      return "YOU WIN!";
+    } else if (userPickObj.name === "drought" && computerPickObj.name === "water") {
+      return "YOU WIN!";
+    } else if (userPickObj.name === "water" && computerPickObj.name === "fire") {
+      return "YOU WIN!";
+    } else {
+      return "YOU LOSE..";
+    }
   };
 
-  const computerPlay = () => {
-    const itemArray = Object.keys(choices);
-    console.log("itemArray?", itemArray);
-    const randomChoice = Math.floor(Math.random() * itemArray.length);
-    const final = itemArray[randomChoice];
-    console.log("random??", randomChoice);
-    setComputerSelect(choices[final]);
+  const play = (userChoice) => {
+    setUserSelect(null);
+    setComputerSelect(null);
+    setResult("");
+    setCardResetKey((prev) => prev + 1);
+
+    //userPlay()
+    const userPickObj = choices[userChoice];
+    setUserSelect(userPickObj);
+    setBgImage(userPickObj.bg);
+
+    //computerPlay() >> delay 1.2s
+    setTimeout(() => {
+      const itemArray = Object.keys(choices);
+      const randomChoice = Math.floor(Math.random() * itemArray.length);
+      const computerPickObj = choices[itemArray[randomChoice]];
+      setComputerSelect(computerPickObj);
+
+      //judge who wins
+      const gameResult = whoIsWin(userPickObj, computerPickObj);
+      setResult(gameResult);
+    }, 1200);
   };
 
   return (
     <div
-      className="flex flex-col justify-center items-center h-screen w-screen"
+      className="flex flex-col justify-center items-center h-screen w-screen gap-y-5 md:gap-y-2"
       style={{
         backgroundImage: bgImage ? `url(${bgImage})` : "none",
         backgroundSize: "cover",
@@ -55,30 +82,37 @@ const App = () => {
       }}
     >
       <div className="flex gap-x-5 md:gap-x-20">
-        <Box title="you" item={userSelect} />
-        <Box title="computer" item={computerSelect} />
+        <Box
+          title="you"
+          item={userSelect}
+          resetKey={cardResetKey}
+          userLose={result === "YOU LOSE.."}
+          tie={result === "DRAW"}
+        />
+        <Box
+          title="computer"
+          item={computerSelect}
+          resetKey={cardResetKey}
+          userWin={result === "YOU WIN!"}
+          tie={result === "DRAW"}
+        />
       </div>
-      <div className="flex gap-x-5 bg-gray-600 rounded-full p-3 md:p-5 mt-30 md:mt-1">
+      <div className="bg-gray-600 text-white text-md md:text-2xl w-30 h-10 md:w-40 md:h-15 rounded-full flex justify-center items-center">
+        {result}
+      </div>
+
+      <div className="flex gap-x-5 bg-gray-600 rounded-full p-3 md:p-5">
         <button
           className="bg-[url('/images/fire-button.png')] bg-cover rounded-full w-15 h-15 md:w-24 md:h-24 cursor-pointer hover:scale-110"
-          onClick={() => {
-            userPlay("fire");
-            computerPlay();
-          }}
+          onClick={() => play("fire")}
         />
         <button
           className="bg-[url('/images/drought-button.png')] bg-cover rounded-full w-15 h-15 md:w-24 md:h-24 cursor-pointer hover:scale-110"
-          onClick={() => {
-            userPlay("drought");
-            computerPlay();
-          }}
+          onClick={() => play("drought")}
         />
         <button
           className="bg-[url('/images/water-button.png')] bg-cover rounded-full w-15 h-15 md:w-24 md:h-24 cursor-pointer hover:scale-110"
-          onClick={() => {
-            userPlay("water");
-            computerPlay();
-          }}
+          onClick={() => play("water")}
         />
       </div>
     </div>
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -2,12 +2,24 @@ import React from "react";
 import "animate.css";
 
 const Box = (props) => {
-  console.log("props", props);
+  const { item, resetKey, userWin, userLose, tie } = props;
+
+  let animation = "animate__fadeIn";
+  if (userWin) {
+    animation = "animate__delay-1s animate__fadeOutRight";
+  } else if (userLose) {
+    animation = "animate__delay-1s animate__fadeOutLeft";
+  } else if (tie) {
+    animation = "animate__delay-1s animate__fadeOutUp";
+  }
 
   return (
     <div className="flex flex-col items-center">
-      <div className="rounded-lg w-40 h-60 md:h-130 md:w-60 mb-5 md:mb-10 animate__animated animate__bounceInLeft">
-        <img src={props?.item?.img} />
+      <div
+        key={`${item?.name} - ${resetKey}`}
+        className={`rounded-lg w-40 h-85 md:h-130 md:w-60 mb-5 md:mb-10 animate__animated ${animation}`}
+      >
+        <img src={item?.img} />
       </div>
     </div>
   );
